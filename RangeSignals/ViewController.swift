933576b606a0265847e6532df86c0f1ec5277c71
Michael Kilhullen <michael.kilhullen@comcast.net>
2017-01-14 17:02:57 -0500

    Initial Commit

---
+++ RangeSignals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RangeSignals
+//
+//  Created by Michael Kilhullen on 1/14/17.
+//  Copyright © 2017 Kilhullenhome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
