88f04f8f54815b886ad5acd7ed4ca8a646298915
Michael Kilhullen <michael.kilhullen@comcast.net>
2017-01-21 16:07:09 -0500

    timer pausing with adjustment utilities.  Disabling buttons visually.  docs

--- RangeSignals/SettingsTableViewController.swift
+++ RangeSignals/SettingsTableViewController.swift
@@ -16,17 +16,31 @@ class SettingsTableViewController: UITableViewController {
     
     var soundEffect: String!
     
+    // MARK: - Actions
+
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+        updateTimeLabel(Int(sender.value))
+    }
+    
+    @IBAction func timerSwitched(_ sender: UISwitch) {
+        
+        stepper.isEnabled = sender.isOn
+        updateTimeLabel(Int(stepper.value))
+        
+    }
+ 
+    // MARK: - Overrides
+    
+    /// load the user defaults; set default value if none exist yet
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // process the defaulta and set values
-        // if this is the first time
         
         let defaults = UserDefaults.standard
         
         timerSwitch.isOn = defaults.bool(forKey: "RangeCommands.timerSwitch")
         let seconds = defaults.integer(forKey: "RangeCommands.seconds")
-
+        
         if let sound = defaults.string(forKey: "RangeCommands.sound") {
             soundEffect = sound
         } else {
@@ -47,41 +61,7 @@ class SettingsTableViewController: UITableViewController {
         }
         
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func stepperValueChanged(_ sender: UIStepper) {
-        
-        updateTimeLabel(Int(sender.value))
-    }
     
-    fileprivate func updateTimeLabel(_ withSeconds: Int) {
-        
-        if (!timerSwitch.isOn) {
-            timeLabel.text = nil
-            return
-        }
-        
-        var minutes, seconds: Int
-        
-        minutes = withSeconds / 60
-        seconds = withSeconds - minutes * 60
-        
-        timeLabel.text = "\(minutes):\(String(format: "%02d", seconds))"
-
-    }
-    
-    @IBAction func timerSwitched(_ sender: UISwitch) {
-    
-        stepper.isEnabled = sender.isOn
-        
-        updateTimeLabel(Int(stepper.value))
-        
-    }
-    // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
@@ -106,6 +86,7 @@ class SettingsTableViewController: UITableViewController {
 
     }
     
+    /// save defaults 
     override func viewWillDisappear(_ animated: Bool) {
         let defaults = UserDefaults.standard
         defaults.set(timerSwitch.isOn , forKey: "RangeCommands.timerSwitch")
@@ -118,8 +99,40 @@ class SettingsTableViewController: UITableViewController {
         defaults.set(seconds, forKey: "RangeCommands.seconds")
         defaults.set(soundEffect, forKey: "RangeCommands.sound")
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.accessoryType = .checkmark;
+        soundEffect = getSoundName(indexPath.row)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.accessoryType = .none
+    }
   
-    fileprivate func selectSound(){
+    // MARK: - class methods
+    
+    /// update the time label to look like a time value
+    func updateTimeLabel(_ withSeconds: Int) {
+        
+        if (!timerSwitch.isOn) {
+            timeLabel.text = nil
+            return
+        }
+        
+        var minutes, seconds: Int
+        
+        minutes = withSeconds / 60
+        seconds = withSeconds - minutes * 60
+        
+        timeLabel.text = "\(minutes):\(String(format: "%02d", seconds))"
+        
+    }
+    
+    /// translate standard sound name to its row
+    func selectSound(){
         var row: Int
         switch soundEffect! {
         case "buzzer": row = 0
@@ -131,7 +144,8 @@ class SettingsTableViewController: UITableViewController {
         tableView.delegate?.tableView!(tableView, didSelectRowAt: indexPath)
     }
     
-    fileprivate func getSoundName(_ row: Int) -> String {
+    /// translate the row back to the standard sound name
+    func getSoundName(_ row: Int) -> String {
         switch row {
         case 0: return "buzzer"
         case 1: return "buzzer2"
@@ -139,6 +153,7 @@ class SettingsTableViewController: UITableViewController {
         }
     }
     
+    /// only one sound can be picked so clear all accessories
     func deSelectSound(){
       
         for row in 0...2 {
@@ -148,15 +163,5 @@ class SettingsTableViewController: UITableViewController {
         }
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let cell = tableView.cellForRow(at: indexPath)
-        cell?.accessoryType = .checkmark;
-        soundEffect = getSoundName(indexPath.row)
- 
-    }
-    
-    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        let cell = tableView.cellForRow(at: indexPath)
-        cell?.accessoryType = .none
-    }
+
 }
--- RangeSignals/ViewController.swift
+++ RangeSignals/ViewController.swift
@@ -6,35 +6,89 @@
 //  Copyright © 2017 Kilhullenhome. All rights reserved.
 //
 
+// TODO: toggle button display to indicate that it's not enabled
+
 import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
 
     var player: AVAudioPlayer?
-    var timer: Timer!
+    var timer: Timer?
     var timerEnabled = false
     var seconds = 120
     var timeleft = 120
     var soundEffect: String?
+    var timerPaused = false
     
+    @IBOutlet weak var lineButton: UIButton!
+    @IBOutlet weak var shootButton: UIButton!
+    @IBOutlet weak var retrieveButton: UIButton!
+    @IBOutlet weak var ceasefireButton: UIButton!
     @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var pauseLabel: UILabel!
+    @IBOutlet weak var adjustTimerStepper: UIStepper!
+    @IBOutlet weak var resetTimerButton: UIButton!
+    @IBOutlet weak var optionsButton: UIBarButtonItem!
 
     // MARK: - Actions
+    
+    /// prepare to shoot makes two sounds
     @IBAction func prepareButton(_ sender: UIButton) {
         playSound(1)
     }
+    
+    /// shoot button makes one sound and starts/resumes the time if enabled
     @IBAction func shootButton(_ sender: UIButton) {
         playSound(0)
-        startTimer()
+        if timerEnabled {
+            if timerPaused {
+                resumeTimer()
+            } else {
+                startTimer()
+            }
+        }
     }
+    // retrieve button makes three tones and resets the timer if enabled
     @IBAction func retrieveAction(_ sender: Any) {
         playSound(2)
-        resetTimer()
+        if timerEnabled {
+         resetTimer()
+        }
     }
     
+    /// cease fire button makes 5 more rapid tones and pauses the timer if enabled
     @IBAction func ceaseButton(_ sender: UIButton) {
         playSound(4)
+        if timerEnabled {
+            if timer != nil {
+                pauseTimer()
+            }
+        }
+    }
+    
+    // action to pause the timer without sending tones
+    @IBAction func timerTapped(_ sender: UITapGestureRecognizer) {
+        if timerEnabled {
+            if timerPaused {
+                resumeTimer()
+            } else {
+                pauseTimer()
+            }
+        }
+        
+    }
+    
+    /// when the timer is paused, the user can optionally add or remove time 
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        timeleft = Int(sender.value)
+        updateTimerLabel(timeleft)
+    }
+    
+    // when timer is paused, the user can optionally reset the time
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        timeleft = seconds
+        updateTimerLabel(timeleft)
     }
     
     // MARK: - overrides
@@ -42,8 +96,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //setup the stepper
+        adjustTimerStepper.minimumValue = 5
+        adjustTimerStepper.maximumValue = 300
+        adjustTimerStepper.stepValue = 1.0
+        
     }
     
+    /// gets defaults and sets up the ui
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
@@ -57,25 +117,13 @@ class ViewController: UIViewController {
             timerEnabled = false
             timerLabel.text = "unlimited time"
         }
+        pauseLabel.text = nil 
         soundEffect = defaults.string(forKey: "RangeCommands.sound")
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-        if timer == nil {
-            return true
-        } else if timer.isValid {
-            return false
-        }
-        return true
     }
     
     // MARK: - methods
 
+    /// plays a sound a certain number of times
     func playSound(_ thisManyTimes: Int) {
         guard let url = Bundle.main.url(forResource: soundEffect, withExtension: "mp3") else {
             print("url not found")
@@ -112,45 +160,97 @@ class ViewController: UIViewController {
         }
     }
     
-    /// start thie timer
-    fileprivate func startTimer(){
-        if timerEnabled {
+    /// start the timer
+    func startTimer(){
+        if timerPaused{
+            resumeTimer()
+        } else {
             timeleft = seconds
             timer = Timer.scheduledTimer(timeInterval: 1.0 , target: self, selector: #selector(timerRunning), userInfo: nil, repeats: true)
         }
+        //once the timer starts, disable  buttons
+        lineButton.isEnabled = false
+        shootButton.isEnabled = false
+        optionsButton.isEnabled = false
+        
+    }
+    
+    /// pause the timer
+    func pauseTimer(){
+        timerPaused = true
+        pauseLabel.text = "PAUSED"
+        pauseLabel.startBlink()
+        lineButton.isEnabled = true
+        shootButton.isEnabled = true
+        retrieveButton.isEnabled = false
+        resetTimerButton.isHidden = false
+        adjustTimerStepper.value = Double(timeleft)
+        adjustTimerStepper.isHidden = false
+   }
+    
+    /// resume the timer
+    func resumeTimer(){
+        timerPaused = false
+        pauseLabel.text = nil
+        pauseLabel.stopBlink()
+        lineButton.isEnabled = false
+        shootButton.isEnabled = false
+        retrieveButton.isEnabled = true
+        resetTimerButton.isHidden = true
+        adjustTimerStepper.isHidden = true
     }
     
     /// timer selector
     func timerRunning() {
-        timeleft -= 1
-        if timeleft == 0 {
-           retrieveAction(timerLabel)
-            resetTimer()
-            return
+        if !timerPaused {
+            timeleft -= 1
+            if timeleft == 0 {
+                retrieveAction(timerLabel)
+                resetTimer()
+                return
+            }
+            updateTimerLabel(timeleft)
         }
-        
-        updateTimerLabel(timeleft)
     }
     
-    /// update the timer label to appear as a clock
-    fileprivate func updateTimerLabel(_ timeInSeconds: Int){
+    /// reset the timer
+    func resetTimer() {
+        if timerEnabled && timer != nil {
+            timer!.invalidate()
+            timer = nil
+            updateTimerLabel(seconds)
+            lineButton.isEnabled = true
+            shootButton.isEnabled = true
+            optionsButton.isEnabled = true
+            
+        }
+        
+    }
+    
+    /// update the timer label to appear like a clock
+    func updateTimerLabel(_ timeInSeconds: Int){
         
         var minutes, seconds: Int
-        
         minutes = (timeInSeconds / 60 )
         seconds = timeInSeconds - minutes * 60
         
         timerLabel.text = "\(String(format: "%02d", minutes)):\(String(format: "%02d", seconds))"
         
     }
-    /// reset the timer
-    fileprivate func resetTimer() {
-        if timerEnabled && timer != nil {
-            timer.invalidate()
-            updateTimerLabel(seconds)
-        }
-        
+  
+    
+}
+
+/// extension to make UILabel blink
+extension UILabel {
+    
+    func startBlink() {
+        UIView.animate(withDuration: 0.8, delay: 0.0, options: [.autoreverse, .repeat], animations: {self.alpha = 0 }, completion: nil)
     }
     
+    func stopBlink() {
+        alpha = 1
+        layer.removeAllAnimations()
+    }
 }
 
